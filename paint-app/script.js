0d6a75f5af4ede3807c6cfa7af35bade58a8a945
Fernanda Najmi <fernandanadjmijr@gmail.com>
2025-11-19 22:35:24 +0700

    little-change:<

---
+++ paint-app/script.js
@@ -0,0 +1,8 @@
+const brush = document.getElementById('brush')
+const colorInput = document.getElementById('colorInput')
+const eraser = document.getElementById('eraser')
+const canvas = document.querySelector('.canvas')
+
+let selectedColor = 0;
+
+
