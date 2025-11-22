19553ecf51c83294bc6e3e1e6ffba0b87b8831fc
Fernanda Najmi <fernandanadjmijr@gmail.com>
2025-11-22 23:01:53 +0700

    lil-change-continueTomorrow :)

--- paint-app/script.js
+++ paint-app/script.js
@@ -1,11 +1,48 @@
-const brush = document.getElementById('brush')
+const brushBtn = document.getElementById('brush')
 const colorInput = document.getElementById('colorInput')
-const eraser = document.getElementById('eraser')
+const eraserBtn = document.getElementById('eraser')
 const canvas = document.querySelector('.canvas')
+const clearBtn = document.getElementById('clear')
 const textColor = document.getElementById('color')
-let selectedColor = colorInput;
-function text(){
-    const paintColor = selectedColor.style.color = colorInput
-    const textColor = 
+
+const ctx =  canvas.getContext('2d')
+
+canvas.width = 700
+canvas.height = 200
+
+let painting = false
+let erasing = false
+let lineWidth = 5
+let colorDefault = "#000000"
+
+function startPosition(e){
+    painting = true;
+    draw(e)
 }
+
+function endPosition(e){
+    painting = false
+    ctx.beginPath()
+}
+
+function draw(){
+    if(!painting) return
+
+    ctx.lineWidth = lineWidth
+    ctx.lineCap = 'round'
+    ctx.strokeStyle = erasing ? '#eeeeee':colorDefault
+
+    ctx.lineTo(clientX - )
+}
+
+function selectBrush(e){
+    eraserBtn.classList.add('active')
+}
+
+function selectColor(e){
+    let selectedColor = e.target.value;
+    textColor.style.color = selectedColor;
+    textColor.textContent = selectedColor;
+}
+colorInput.addEventListener('input',selectColor)
 
