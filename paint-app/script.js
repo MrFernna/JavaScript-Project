86e65d3f82f1e53cceac1bb9884fca2a9000b926
Fernanda Najmi <fernandanadjmijr@gmail.com>
2025-11-24 21:51:52 +0700

    finishProject

--- paint-app/script.js
+++ paint-app/script.js
@@ -7,8 +7,10 @@ const textColor = document.getElementById('color')
 
 const ctx =  canvas.getContext('2d')
 
+let selectedColor;
+
 canvas.width = 700
-canvas.height = 200
+canvas.height = 400
 
 let painting = false
 let erasing = false
@@ -25,23 +27,44 @@ function endPosition(e){
     ctx.beginPath()
 }
 
-function draw(){
-    if(!painting) return
+function draw(e){
+    if(!painting) return 
 
     ctx.lineWidth = lineWidth
     ctx.lineCap = 'round'
-    ctx.strokeStyle = erasing ? '#eeeeee':colorDefault
+    ctx.strokeStyle = erasing ? '#ffffff': selectedColor
 
+    ctx.lineTo(e.clientX - canvas.offsetLeft, e.clientY - canvas.offsetTop)
+    ctx.stroke()
+    ctx.beginPath()
+    ctx.moveTo(e.clientX - canvas.offsetLeft, e.clientY - canvas.offsetTop)
 }
 
 function selectBrush(e){
+    erasing = false;
+    brushBtn.classList.add('active')
+    eraserBtn.classList.remove('active')
+}
+function selectEraser(e){
+    erasing = true
     eraserBtn.classList.add('active')
+    brushBtn.classList.remove('active')
 }
 
 function selectColor(e){
-    let selectedColor = e.target.value;
+    selectedColor = e.target.value;
     textColor.style.color = selectedColor;
     textColor.textContent = selectedColor;
 }
-colorInput.addEventListener('input',selectColor)
+function clear(){
+    ctx.clearRect(0,0, canvas.width, canvas.height)
+}
 
+canvas.addEventListener('mousedown',startPosition)
+canvas.addEventListener('mouseup',endPosition)
+canvas.addEventListener('mouseout',endPosition)
+canvas.addEventListener('mousemove',draw)
+colorInput.addEventListener('input',selectColor)
+brushBtn.addEventListener('click',selectBrush)
+eraserBtn.addEventListener('click',selectEraser)
+clearBtn.addEventListener('click',clear)
