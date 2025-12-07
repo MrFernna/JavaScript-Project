31155b33e680d008d9602b448aa7139d769a9141
Fernanda Najmi <fernandanadjmijr@gmail.com>
2025-12-07 23:10:41 +0700

    lil-changeAgain

--- audio-player/script.js
+++ audio-player/script.js
@@ -1,13 +1,23 @@
+//untuk mengambil data playlist dari local storage
+let playlist = JSON.parse(localStorage.getItem('playlist'))
+
 const playIcon = document.getElementById('play-icon')
 const pauseIcon = document.getElementById('pause-icon')
 const playBtn = document.getElementById('play-pause')
+const progressBar = document.getElementById('progress-bar')
+const trackName = document.getElementById('selectedAudio')
+const audio = document.getElementById('audio')
+const inputFile = document.getElementById('inputFile')
+const playlistSection = document.getElementById("playlist")
 
-
-playIcon.addEventListener('click', ()=>{
-    playIcon.style.display = "none";
-    pauseIcon.style.display = "block"
-})
-pauseIcon.addEventListener('click', ()=>{
-    playIcon.style.display = "block";
-    pauseIcon.style.display = "none"
-})
+function createPlayList(){
+    playlistSection.innerHTML = ""
+    playlist.forEach((track, index) => {
+        const li = document.createElement('li')
+        li.textContent = track.title
+        li.dataset.src = track.src
+    })
+    li.addEventListener('click',()=>{
+        audio.src = track.src
+    })
+}
