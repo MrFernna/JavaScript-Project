a265664313e930b01fc9fa3253ca726bba3199ab
Fernanda Najmi <fernandanadjmijr@gmail.com>
2025-12-27 00:04:29 +0700

    bruh

---
+++ troll-button/script.js
@@ -0,0 +1,10 @@
+const clickBtn = document.getElementById('clickBtn')
+let margin = 0;
+let marginBottom = 0;
+
+function randomMove(){
+    const randomMargin = Math.floor(Math.random(margin)* 100)
+    clickBtn.style.margin = randomMargin + '%'
+}
+
+clickBtn.addEventListener('mouseover',randomMove)
