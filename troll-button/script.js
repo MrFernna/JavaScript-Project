60cd2db14a435ff4ecd8b3c1d181c19f98ab6c0a
Fernanda Najmi <fernandabaru13@gmail.com>
2025-12-28 15:10:02 +0700

    test

--- troll-button/script.js
+++ troll-button/script.js
@@ -6,5 +6,5 @@ function randomMove(){
     const randomMargin = Math.floor(Math.random(margin)* 100)
     clickBtn.style.margin = randomMargin + '%'
 }
-
+//test
 clickBtn.addEventListener('mouseover',randomMove)
