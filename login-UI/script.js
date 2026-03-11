7efacde9f2f45de1a8e0a3d109ef30e48125fccd
Fernanda Najmi <fernandabaru13@gmail.com>
2026-03-11 23:53:35 +0700

    absen

--- login-UI/script.js
+++ login-UI/script.js
@@ -1,14 +1,12 @@
-//absen dlu
-//absen dlu
-//absen dlu
-//absen dlu
-//absen dlu
-//absen dlu
-//absen dlu
-//absen dlu
-//absen dlu
-//absen dlu
-//absen dlu
-//absen dlu
-//absen dlu
-//absen dlu
+const inputUsername = document.querySelector(".input-username")
+const inputPassword = document.querySelector(".input-password")
+const EnterBtn = document.querySelector(".button")
+
+
+
+EnterBtn.addEventListener('click',()=>{
+    console.log(`Username: ${inputUsername.value}`)
+    console.log(`Password: ${inputPassword.value}`)
+    inputUsername.value = ""
+    inputPassword.value = ""
+})
